23a4ed81d0544ad9eae2cdcf3d7f1e093dbc75e0
hrshuv0 <shuvo@a4aero.com>
2024-04-10 16:35:07 +0600

    scaffold

--- src/CustomUserIdentityApp/Program.cs
+++ src/CustomUserIdentityApp/Program.cs
@@ -1,4 +1,15 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using CustomUserIdentityApp.Data;
+using CustomUserIdentityApp.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("CustomUserIdentityAppContextConnection") ?? throw new InvalidOperationException("Connection string 'CustomUserIdentityAppContextConnection' not found.");
+
+builder.Services.AddDbContext<CustomUserIdentityAppContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<CustomAppUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<CustomUserIdentityAppContext>();
+
+builder.Services.AddRazorPages();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -18,10 +29,13 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
